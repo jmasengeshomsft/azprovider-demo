bbfe10a7b8df0f1c3a21b33a723a6cb223c911d3
jmasengeshomsft <86074746+jmasengeshomsft@users.noreply.github.com>
2023-06-09 09:52:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-# data "azurerm_api_management" "jm_apim" {
-#   name                = "jmdevapim001"
-#   resource_group_name = "apim-demos"
-# }
-
-# resource "azapi_resource" "myfragment" {
-#   type = "Microsoft.ApiManagement/service/policyFragments@2022-08-01"
-#   name = "jmNewFragment"
-#   parent_id = data.azurerm_api_management.jm_apim.id
-#   body = jsonencode({
-#     properties = {
-#       description = "string"
-#       format = "xml"
-#       value = <<EOT
-#       <fragment>
-#         <set-header name="x-request-context-data" exists-action="override">
-#           <value>@(context.User.Id)</value>
-#           <value>@(context.Deployment.Region)</value>
-#         </set-header>
-#      </fragment>
-#      EOT
-#     }
-#   })
-# }
 
 provider "azurerm" {
     features {}
